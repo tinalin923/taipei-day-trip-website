b60384226d090fda605b23d62a1a2f47e9668c32
tinalin923 <tinalin923@gmail.com>
2022-03-04 11:00:50 +0800

    change host

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 class Config():
-    SECRET_KEY = environ.get('SECRET_KEY')
+    # SECRET_KEY = environ.get('SECRET_KEY')
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
     
@@ -15,6 +15,7 @@ class DevConfig(Config):
     DEBUG = True
     TESTING = True
     
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
-    SQLALCHEMY_DATABASE_URI = environ.get('DEV_DATABASE_URI')
+    # SQLALCHEMY_TRACK_MODIFICATIONS=False
+    # SQLALCHEMY_DATABASE_URI = environ.get('DEV_DATABASE_URI')
+
 
