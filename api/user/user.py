2eb1ebde4b8f959f23056299e5d3a087b8138bbd
tinalin923 <tinalin923@gmail.com>
2022-03-22 23:23:29 +0800

    finish user api*4

--- api/user/user.py
+++ api/user/user.py
@@ -1,4 +1,4 @@
-from flask import Flask,Blueprint,session,jsonify,request
+from flask import Blueprint,make_response,jsonify,request
 from flask_restful import Api, Resource
 from ..mydb import cnxpool
 import datetime
@@ -7,6 +7,7 @@ import os
 from dotenv import load_dotenv   
 load_dotenv()  
 secret = os.getenv('SECRET')
+
 
 user_bp = Blueprint("user_name",__name__)
 
@@ -17,47 +18,78 @@ cnx = cnxpool.get_connection()
 
 class User(Resource):
     def get(self):
-
-        response = jsonify({
-            "data":{
-                "id": "1",
-                "name": "qqq",
-                "email": "qqq"
-            }
-        }),200
-        return response
-
-
+        token = request.cookies.get('user')
+        print(token)
+        if not token:
+            response = make_response(jsonify({
+                "message":"尚未登入"
+            }),400)
+            return response
+        else:
+            user = jwt.decode(token,secret,algorithms = ['HS512'])
+            id = user['id']
+            name = user['name']
+            email = user['email']
+            response = make_response(jsonify({
+                "data":{
+                    "id": id,
+                    "name": name,
+                    "email": email
+                }
+            }),200)
+            return response
 
 
     def post(self):       #註冊
-        name = request.form["name"]
-        email = request.form["email"]
-        password = request.form["passsword"]
+        try:    
+            req = request.get_json()
+            reqName = req["name"]
+            reqEmail = req["email"]
+            reqPassword = req["password"]
 
-        if not name or not email or not password:
-            response = jsonify({'message' : "請輸入資料"})
-            return  response
+            if not reqName or not reqEmail or not reqPassword:
+                response = make_response(jsonify({
+                    "error": True,
+                    "message" : "請輸入資料"
+                }),400)
+                return  response
 
+            else:
+                cnx = cnxpool.get_connection()
+                cursor = cnx.cursor()
+                query = (
+                    'INSERT INTO `members` (`name`, `email`, `password`) '
+                    'VALUES (%s, %s, %s) '
+                    'ON DUPLICATE KEY UPDATE `email` = VALUES(email)'
+                    )
+                params = (reqName, reqEmail, reqPassword)
+                cursor.execute(query,params)
+                cnx.commit()
+                newId = cursor.lastrowid
+                cursor.close()
+                if not newId:  
+                    cnx.close()
+                    response = make_response(jsonify({
+                        "error": True,
+                        "message": "Email已被註冊"
+                    }),400)
+                    return response
+                    
+                else:       #可以註冊
+                    
+                    cnx.close()
+                    response = make_response(jsonify({
+                        "ok": True
+                    }),200)
+                    return response
 
+        except Exception as e:
+            response = make_response(jsonify({
+                "error": True,
+                "message": e
+            }),500)
 
-
-        response = jsonify({
-            "ok": True
-        }),200
-
-        response = jsonify({
-            "error": True,
-            "message": "自訂訊息"
-        }),400
-
-        response = jsonify({
-            "error": True,
-            "message": "自訂訊息"
-        }),500
-
-        return response
-
+            return response
 
 
     def patch(self):
@@ -67,56 +99,62 @@ class User(Resource):
             reqPassword = req["password"]
 
             if not reqEmail or not reqPassword:
-                response = jsonify({
+                response = make_response(jsonify({
                     "error": True,
                     "message": "請輸入帳號密碼"
-                }),400
+                }),400)
                 return response
             else:    
                 cnx = cnxpool.get_connection()
                 cursor = cnx.cursor()
-                query = ('SELECT `password` FROM `members` WHERE `email` = %s ')
+                query = ('SELECT `id`,`name`,`password` FROM `members` WHERE `email` = %s ')
                 params = (reqEmail,)
                 cursor.execute(query,params)
-                password = cursor.fetchone()
+                user = cursor.fetchone()   # password = ('123',)
                 cursor.close()
                 cnx.close()
-                if not password: 
-                    response = jsonify({
+                if not user: 
+                    response = make_response(jsonify({
                     "error": True,
                     "message": "登入失敗，查無此帳號"
-                    }),400
+                    }),400)
                     return response
-
-                elif reqPassword == password:
-                    token = jwt.endoce({"email":reqEmail}, secret, algorithm = 'HS256')
-                    #把token存到cookie
-                    response.set_cookie(key = 'user', value = token, expires = datetime.datetime.utcnow() + datetime.timedelta(seconds=15))
-                    response = jsonify({
-                        "ok": True
-                    }),200
-                    return response
-                else:
-                    response = jsonify({
+                elif reqPassword != user[2]:
+                    
+                    response = make_response(jsonify({
                     "error": True,
                     "message": "登入失敗，帳號密碼錯誤"
-                    }),400
+                    }),400)
                     return response
+                else:
+                    print(user[2])
+                    print(reqPassword)
+                    message = {"id":user[0], "name":user[1], "email":reqEmail}
+                    token = jwt.encode(message, secret, algorithm = 'HS512')
+                    response = make_response(jsonify({
+                        "ok": True
+                    }),200)
+                    #把token存到cookie
+                    response.set_cookie(key = 'user', value = token, expires = datetime.datetime.utcnow() + datetime.timedelta(seconds=15))
+                    return response
+                
             
         except Exception as e:    
-            response = jsonify({
+            response = make_response(jsonify({
                 "error": True,
                 "message": e
-            }),500
+            }),500)
 
             return response
 
 
         
     def delete(self):
-        response = jsonify({
+        
+        response = make_response(jsonify({
             "ok": True
-        }),200
+        }),200)
+        response.set_cookie(key='user', value='', expires=0)
         return response
 
 
