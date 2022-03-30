9d2307e695087ebfd95b00911af0f9303867ed93
tinalin923 <tinalin923@gmail.com>
2022-03-30 16:55:49 +0800

    complete delete api with sql

--- api/booking/booking.py
+++ api/booking/booking.py
@@ -15,7 +15,6 @@ api = Api(booking_bp)
 class Booking(Resource):
     def get(self):
         token = request.cookies.get("user")
-        print(token)
         if not token:
             res = {
                 "error":True,
@@ -26,6 +25,7 @@ class Booking(Resource):
             status = 200
             user = jwt.decode(token, secret, algorithms = ['HS512'])
             email = user['email']
+            print("28",email)
             cnx = cnxpool.get_connection()
             cursor = cnx.cursor(buffered=True)
             query = ("SELECT `taipei`.`id`, `name`, `address`, `images`, `date`, `time`, `price`"
@@ -39,32 +39,34 @@ class Booking(Resource):
                 cursor.close()
                 cnx.close()
                 # data = list(data)   #不用讓tuple變成list,也可進行split操作
-                print(data)
-                id = data[0]
-                name = data[1]
-                address = data[2]
-                image = data[3]             # 此時的image為字串，不好取出完整的一個image url
-                image = image.split("\n")   # 所以需要 " .split() " 將字串分割成list
-                image = image[0]
-                date = data[4]
-                date = date.isoformat()    #讓date的type(class?)從 datetime.date變成string
-                print(date)
-                print(type(date))
-                time = data[5]
-                price = data[6]
-                res = {
-                    "data":{
-                        "attraction":{
-                            "id":id,
-                            "name":name,
-                            "address":address,
-                            "image":image
-                        },
-                        "date":date,
-                        "time":time,
-                        "price":price
-                    }
+                if data == None:
+                    res = {
+                    "data":None
                 }
+                else:  
+                    id = data[0]
+                    name = data[1]
+                    address = data[2]
+                    image = data[3]             # 此時的image為字串，不好取出完整的一個image url
+                    image = image.split("\n")   # 所以需要 " .split() " 將字串分割成list
+                    image = image[0]
+                    date = data[4]
+                    date = date.isoformat()    #讓date的type(class?)從 datetime.date變成string
+                    time = data[5]
+                    price = data[6]
+                    res = {
+                        "data":{
+                            "attraction":{
+                                "id":id,
+                                "name":name,
+                                "address":address,
+                                "image":image
+                            },
+                            "date":date,
+                            "time":time,
+                            "price":price
+                        }
+                    }
             except Error as e:
                 print("Error:{}".format(e))
                 res = {
@@ -85,12 +87,10 @@ class Booking(Resource):
                     "message":"未登入系統，拒絕存取"
                 }
                 status = 403
-
-
             user = jwt.decode(token, secret, algorithms = ['HS512'])
             email = user['email']
+
             booking_data = request.get_json()
-            print(booking_data)
             attractionId = booking_data['attractionId']
             date = booking_data['date']
             time = booking_data['time']
@@ -100,6 +100,8 @@ class Booking(Resource):
             cursor = cnx.cursor()
             query = ("INSERT INTO `booking` (`email`,`attractionId`,`date`,`time`,`price`)"
                     "VALUES (%s, %s, %s, %s, %s)"
+                    "ON DUPLICATE KEY UPDATE `attractionId`= VALUES(`attractionId`),"
+                    "`date`= VALUES(`date`),`time`= VALUES(`time`),`price`= VALUES(`price`)"
             )
             params = (email,attractionId,date,time,price)
             try:
@@ -141,10 +143,29 @@ class Booking(Resource):
             }
             status = 403
         else:
-            res = {
-                "ok": True
-            }    
-            status = 200
+            user = jwt.decode(token, secret, algorithms = ['HS512'])
+            email = user['email']
+            cnx = cnxpool.get_connection()
+            cursor = cnx.cursor()
+            query = ("DELETE FROM `booking` WHERE `email` = %s")
+            params = (email,)
+            try:
+                cursor.execute(query,params)
+                cursor.close()
+                cnx.commit()
+                cnx.close()
+                res = {
+                    "ok": True
+                }    
+                status = 200
+            except Error as e:
+                res = {
+                    "error":True,
+                    "message":"刪除失敗"
+                }
+                status = 400
+                print("Error: {}".format(e))
+
         response = make_response(jsonify(res),status)
         response.headers['Access-Control-Allow-Origin'] = '*'
         return response
